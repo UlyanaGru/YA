7f3adc73a301973d5f7be2fdb629f8e374536649
UlyanaGru <giperbolicheskiisinys@gmail.com>
2026-06-19 12:22:03 +0300

    0 ms Beats 100 Solution for TwoSum

--- LeetCode/Algoritms/Twosum.py
+++ LeetCode/Algoritms/Twosum.py
@@ -1,2 +1,20 @@
-nums = input()
-target = input()
+def twoSum(nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #Создаем пустой словарь: в будущем добавляем туда "значение:индекс"
+        dict_map = {}
+        for i,num in enumerate(nums):
+            curr = target - num
+            if curr in dict_map: 
+                return [i, dict_map.get(curr)]
+            else:
+                dict_map[nums[i]] = i
+
+if __name__ == '__main__':
+    nums = input().strip().strip(']').strip('[').split(',')
+    nums = list(map(int,nums))
+    target = int(input())
+    print(twoSum(nums, target))
